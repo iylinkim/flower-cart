b0bab78a9be7011b33bc611f0f3cfc0751f8de8e
iylinkim <kiylin94@naver.com>
2021-05-21 21:17:27 +0900

    Add CartItem component and get data

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CartItem = ({ info }) => {
+  const { id, image_url, product_name, product_price, current_count, stock } =
+    info;
+  return (
+    <li className="cart_item">
+      <p className="chk">
+        <input type="checkbox" />
+      </p>
+      <p className="cart_item_img">
+        <img src={image_url} alt={product_name} />
+      </p>
+      <strong className="cart_item_title">{product_name}</strong>
+      <strong className="cart_item_price">{product_price}원</strong>
+      <button className="delete_btn">x</button>
+    </li>
+  );
+};
+
+export default CartItem;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { cartLists } from "data";
+import CartItem from "components/CartItem";
+import React from "react";
 
 const Home = (props) => {
-    return <h1>HOME</h1>
+  return (
+    <div className="cart">
+      <h2>장바구니</h2>
+      <h3>주소</h3>
+      <p>서울시 강남구 도산대로 174 7층</p>
+      <h3>배송방법</h3>
+      <h3>상품 내역</h3>
+      <input type="checkbox" className="all_chk" />
+      <ul className="cart_list">
+        {cartLists.map((info) => {
+          return <CartItem key={info.id} info={info} />;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
